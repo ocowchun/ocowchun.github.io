8e770c894eaef87c4acea3e26467750b37750c1e
ocowchun <ocowchun@gmail.com>
2015-02-27 00:56:29 +0800

    add slot

---
+++ app/javascripts/slot.js
@@ -0,0 +1,34 @@
+'use strict';
+
+$(function() {
+	var $btnMove = $('#btnMove');
+	var $list = $('#list');
+
+	$btnMove.on('click', function() {
+		slotGo();
+	});
+
+	function init() {
+		var html = "";
+		_(20).times(function(n) {
+			html += "<li>" + "<img src='http://www.avatarpro.biz/avatar?=" + n + "' alt='' />" + "</li>"
+		});
+		$list.html(html + html);
+	}
+
+	init();
+
+	var current = 0;
+	var intervalID;
+
+	function slotGo() {
+		$list.css("margin-top", "0px");		
+		var limit = 82 * Math.floor(20 + Math.random() * 20);
+		var position = "-" + limit + "px";
+		$list.animate({
+			'margin-top': position
+		}, 3000, function() {});
+	}
+
+
+})
